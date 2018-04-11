82b5502214c509fb5aa594b36a7274535c80fcaf
Tom Hughes <tomkhughes@g.cofc.edu>
2018-04-10 21:12:31 -0700

    navbar working except logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+
 //import pages here: 
 //import components here: 
 
