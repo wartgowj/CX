206b4943bc84091e5384ea7d2459ec3e6bf50617
eyl91 <elmayl91@gmail.com>
2018-04-12 20:43:27 -0700

    got rates api to display under nav

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 
-
 //import pages here: 
 import Detail from "./pages/Detail";
 import CXPlaces from "./pages/CXPlaces";
 import Home from "./pages/Home"
+import RatesDisplay from "./components/RatesDisplay";
 
 //import components here: 
 
@@ -14,6 +14,7 @@ const App = () => (
   <Router>
     <div>
       <Nav />
+      <RatesDisplay />
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/cxplaces" component={CXPlaces}/>
