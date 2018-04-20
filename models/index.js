abe35e6177a26f1bb9aaeb5f911e6c911d05d82f
Jaime Wartgow <wartgowj@gmail.com>
2018-04-19 19:30:12 -0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
    Cxplace: require("./cxplace"),
    User: require("./user"),
-=======
    Cxplace: require("./cxplace")
->>>>>>> d8df20785337e097e4274321546ffdc11ac38d72
 };
